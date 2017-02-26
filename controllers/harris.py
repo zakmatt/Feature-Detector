d484ff99d7ec4651de3c67a3e532fa430b131bf4
matthew-zak <matthew.zak@tooploox.com>
2017-02-26 14:58:56 -0500

    points matched correctly. TODO: rotation invariance

--- controllers/harris.py
+++ controllers/harris.py
@@ -8,6 +8,8 @@ from scipy.ndimage import filters
 class Harris(object):
     
     def __init__(self, image, window_size, sigma, threshold):
+        #h, w, _ = image.shape
+        #image = cv2.resize(image, (h//2, w//2))
         self.image = image
         self.window_size = window_size
         self.sigma = sigma
@@ -47,6 +49,12 @@ class Harris(object):
             return is_edge_close
         
         gray_image = cv2.cvtColor(self.image, cv2.COLOR_BGR2GRAY)
+        
+        # DoG filtering
+        kernel_dog = d_o_g(21, 1.5, 1.0)
+        gray_image = cv2.filter2D(gray_image, -1, kernel_dog)
+        cv2.imwrite('dog_gray.jpg', gray_image)
+        
         self.I_x, self.I_y = compute_derivatives(gray_image)
         I_xx = self.I_x ** 2
         I_yy = self.I_y ** 2
@@ -55,7 +63,6 @@ class Harris(object):
         h, w, _ = self.image.shape
         kernel = GaussianKernel(self.window_size, self.sigma)
         #kernel = d_o_g(self.window_size, 1, 5)
-        offset = self.window_size // 2
         corner_list = []
         self.key_points = []
         corners_map = np.zeros((h,w))
@@ -77,6 +84,7 @@ class Harris(object):
         self.threshold *= max_value
         suppress_pos = imgH < self.threshold
         imgH[suppress_pos] = 0.0
+        
         # non-maximum suppression in 5x5 regions
         maxH = filters.maximum_filter(imgH, (5,5))
         imgH = imgH * (imgH == maxH)
@@ -87,83 +95,20 @@ class Harris(object):
             y = index[0]
             x = index[1]
             self.key_points.append(cv2.KeyPoint(x, y, 15))
-            if not is_edge_close(h, w, y, x):
-                corner_list.append([y, x, imgH[y, x]])
+            #if not is_edge_close(h, w, y, x):
+            corner_list.append([y, x, imgH[y, x]])
         
         self.corner_list = corner_list
         self.corners_map = corners_map
-        output_image = cv2.drawKeypoints(rescale(self.image).astype('uint8'), self.key_points, self.image)
-        cv2.imwrite(f_name, output_image)
         print(len(corner_list))
-
-    def harris(self, f_name, sigma_d=1.0, sigma_i=1.5, count=512):
-        
-        def is_edge_close(h, w, y, x):
-            is_edge_close = False
-            if (y - 16) <= 0 or (y + 16) >= h:
-                is_edge_close =  True
-            if (x - 16) <= 0 or (x + 16) >= w:
-                is_edge_close =  True
-            
-            return is_edge_close
-        
-        gray_image = cv2.cvtColor(self.image, cv2.COLOR_BGR2GRAY)
-        gray_image = (gray_image).astype(np.float32)
-        h, w = gray_image.shape[0:2]
-    
-        # compute image derivatives
-        Ix = filters.gaussian_filter1d(gray_image, sigma_d, 0, 0)
-        Ix = filters.gaussian_filter1d(Ix, sigma_d, 1, 1)
-        self.I_x = Ix
-        Iy = filters.gaussian_filter1d(gray_image, sigma_d, 1, 0)
-        Iy = filters.gaussian_filter1d(Iy, sigma_d, 0, 1)
-        self.I_y = Iy
-    
-        # compute elements of the structure tensor
-        Ixx = filters.gaussian_filter(Ix**2, sigma_i, 0)
-        Iyy = filters.gaussian_filter(Iy**2, sigma_i, 0)
-        Ixy = filters.gaussian_filter(Ix * Iy, sigma_i, 0)
-    
-        # compute Harris feature strength, avoiding divide by zero
-        imgH = (Ixx * Iyy - Ixy**2) / (Ixx + Iyy + 1e-8)
-    
-        # exclude points near the image border
-        imgH[:16, :] = 0
-        imgH[-16:, :] = 0
-        imgH[:, :16] = 0
-        imgH[:, -16:] = 0
-    
-        max_value = np.max(imgH)
-        self.threshold *= max_value
-        suppress_pos = imgH < self.threshold
-        imgH[suppress_pos] = 0.0
-        # non-maximum suppression in 5x5 regions
-        maxH = filters.maximum_filter(imgH, (3,3))
-        imgH = imgH * (imgH == maxH)
-    
-        max_y, max_x = np.nonzero(imgH)
-        indices = [pos for pos in zip(max_y, max_x)]
-        
-        corner_list = []
-        self.key_points = []
-        for index in indices:
-            y = index[0]
-            x = index[1]
-            self.key_points.append(cv2.KeyPoint(x, y, 15))
-            if not is_edge_close(h, w, y, x):
-                corner_list.append([y, x, imgH[y, x]])
-    
-        self.corner_list = corner_list
         output_image = cv2.drawKeypoints(rescale(self.image).astype('uint8'), self.key_points, self.image)
         cv2.imwrite(f_name, output_image)
-        #print(len(corner_list))
-        
+
 if __name__ == '__main__':
     #image = cv2.imread('../checkerboard.png')
-    #image = cv2.imread('bicycle.bmp')
-    image = open_image('Yosemite1.jpg')
+    image = cv2.imread('im1.jpg')
+    #image = open_image('Yosemite1.jpg')
     gray_image = cv2.cvtColor(image, cv2.COLOR_BGR2GRAY)
     harris = Harris(image, 5, 1, 0.3)
-    harris.harris('lfdsfjs.jpg')
+    harris.harris_matrix('Yosemite1_result.jpg')
     harris.gradient_matrix()
-    print(np.array(harris.corner_list).shape)
