12e306cca8b17f3c891b07595965a7197421d969
matthew-zak <matthew.zak@tooploox.com>
2017-02-26 17:58:12 -0500

    clean harris code

--- controllers/harris.py
+++ controllers/harris.py
@@ -134,64 +134,7 @@ class Harris(object):
         self.corners_map = corners_map
         output_image = cv2.drawKeypoints(rescale(self.image).astype('uint8'), self.key_points, self.image)
         cv2.imwrite(f_name, output_image)  
-    '''
-    def harris_matrix(self, f_name):
-        
-        gray_image = cv2.cvtColor(self.image, cv2.COLOR_BGR2GRAY)
-        
-        # DoG filtering
-        kernel_dog = d_o_g(21, 1.5, 1.0)
-        gray_image = cv2.filter2D(gray_image, -1, kernel_dog)
-        cv2.imwrite('dog_gray.jpg', gray_image)
-        
-        self.I_x, self.I_y = compute_derivatives(gray_image)
-        I_xx = self.I_x ** 2
-        I_yy = self.I_y ** 2
-        I_xy = self.I_x * self.I_y
-        
-        h, w, _ = self.image.shape
-        kernel = GaussianKernel(self.window_size, self.sigma)
-        #kernel = d_o_g(self.window_size, 1, 5)
-        corner_list = []
-        self.key_points = []
-        corners_map = np.zeros((h,w))
-        
-        I_xx = np.array(cv2.filter2D(I_xx, -1, kernel), dtype = np.float32)
-        I_yy = np.array(cv2.filter2D(I_yy, -1, kernel), dtype = np.float32)
-        I_xy = np.array(cv2.filter2D(I_xy, -1, kernel), dtype = np.float32)
-        
-        # compute Harris feature strength, avoiding divide by zero
-        imgH = (I_xx * I_yy - I_xy**2) / (I_xx + I_yy + 1e-8)
-        
-        # exclude points near the image border
-        imgH[:16, :] = 0
-        imgH[-16:, :] = 0
-        imgH[:, :16] = 0
-        imgH[:, -16:] = 0
-            
-        max_value = np.max(imgH)
-        self.threshold *= max_value
-        suppress_pos = imgH < self.threshold
-        imgH[suppress_pos] = 0.0
-        
-        # non-maximum suppression in 5x5 regions
-        maxH = filters.maximum_filter(imgH, (5,5))
-        imgH = imgH * (imgH == maxH)
-        
-        max_y, max_x = np.nonzero(imgH)
-        indices = [pos for pos in zip(max_y, max_x)]
-        for index in indices:
-            y = index[0]
-            x = index[1]
-            self.key_points.append(cv2.KeyPoint(x, y, 15))
-            corner_list.append([y, x, imgH[y, x]])
-        
-        self.corner_list = corner_list
-        self.corners_map = corners_map
-        print(len(corner_list))
-        output_image = cv2.drawKeypoints(rescale(self.image).astype('uint8'), self.key_points, self.image)
-        cv2.imwrite(f_name, output_image)
-    '''
+
 if __name__ == '__main__':
     #image = cv2.imread('../checkerboard.png')
     image = open_image('Yosemite1.jpg')
